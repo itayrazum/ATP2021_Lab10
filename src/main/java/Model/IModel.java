2b65762171841fb6676137862217d30001be0f8d
itayrazum <itayrazum@gmail.com>
2022-07-01 12:28:12 +0300

    last2

--- src/main/java/Model/IModel.java
+++ src/main/java/Model/IModel.java
@@ -1,32 +1,25 @@
 package Model;
 
 import algorithms.mazeGenerators.Maze;
-import algorithms.search.Solution;
 
 import java.net.UnknownHostException;
-import java.util.Observer;
 
 public interface IModel {
-    Maze getMaze();
-    int getColPlayer();
-    int getRowPlayer();
-    void setColPlayer(int col);
-    void setRowPlayer(int row);
-    Solution getSolution();
-    void generateMaze(int row, int col) throws UnknownHostException;
-    void updateLocationP(int direction);
-    void solveMaze(Maze mazeToSolve);
-    void assignObserver(Observer o);
-    void start();
-    void stop() throws InterruptedException;
-    void updateConfig(int threadSize, int rows, int cols, String generateAlg, String solverAlg);
-    String getSolverAlg();
-    String getGenerateAlg();
-    int getRowMaze();
-    int getColMaze();
-    boolean checkFinish();
-    void saveMaze(String name);
-    void writeErrorToLog();
 
-    int getThreadsNum();
+    /** This function generate a maze by the server, according client's request
+     * @param rows represents number of rows in maze
+     * @param cols represents number of columns in maze
+     * @throws UnknownHostException
+     */
+    void generateMaze(int rows, int cols) throws UnknownHostException;
+
+    /**
+     * @return the maze
+     */
+    Maze getMaze();
+
+    public void setMaze(Maze maze);
+
+
+    void stopServers() throws InterruptedException;
 }
